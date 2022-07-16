eba4bca7e5ef01c990a06e8cd268ef346a56001e
Lucas Souza <lucascorreia325@gmail.com>
2022-07-16 20:03:57 -0300

    Sale listing

--- FrontEnd/src/components/SalesCard/index.tsx
+++ FrontEnd/src/components/SalesCard/index.tsx
@@ -9,6 +9,9 @@ import ptBR from "date-fns/locale/pt-BR";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
+
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
@@ -17,10 +20,12 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
+        axios.get(`${BASE_URL}/sales`)
             .then(response => {
-                console.log(response.data);
+                setSales(response.data.content);
             })
     }, []);
     
@@ -65,45 +70,23 @@ function SalesCard() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#1</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>LM</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#2</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>LM</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#2</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>LM</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })}
                         </tbody>
                     </table>
                 </div>
@@ -113,3 +96,7 @@ function SalesCard() {
 }
 
 export default SalesCard;
+function setSales(content: any) {
+    throw new Error("Function not implemented.");
+}
+
